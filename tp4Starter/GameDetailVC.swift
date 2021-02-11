ba8adb9137e6c14d241763b844fbd9ecdf8bf4b7
kecsou <kevin.voyer.developpeur@gmail.com>
2021-02-11 14:49:42 +0100

    feat: add tabViewController and ViewController

---
+++ tp4Starter/GameDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  GameDetailVC.swift
+//  tp4Starter
+//
+//  Created by Kévin VOYER on 11/02/2021.
+//
+
+import UIKit
+
+class GameDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
