3ab095f6234d298b619685c97dce7519f3185c10
kecsou <kevin.voyer.developpeur@gmail.com>
2021-02-11 14:34:12 +0100

    Initial Commit

---
+++ tp4Starter/Game.swift
@@ -0,0 +1,26 @@
+//
+//  Game.swift
+//  tp4Starter
+//
+//  Created by Benoit Verdier on 11/02/2021.
+//
+
+import Foundation
+
+class Game: Decodable {
+    var name: String
+    var id: Int
+    var smallImageURL: String
+    var bigImageURL: String
+    var score: Int
+    var platform: String
+
+    init(name: String, id:Int, smallImageURL: String, bigImageURL: String, score: Int, platform: String) {
+        self.name = name
+        self.id = id
+        self.smallImageURL = smallImageURL
+        self.bigImageURL = bigImageURL
+        self.score = score
+        self.platform = platform
+    }
+}
